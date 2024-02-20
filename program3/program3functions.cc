53b04206f9b8841728842c37ae890e8af9dd33fc
PakshP <pakshpatel1@gmail.com>
2024-02-20 11:54:56 -0500

    Finished ShiftArray

--- program3/program3functions.cc
+++ program3/program3functions.cc
@@ -20,3 +20,22 @@ void RemoveValues(int array[], int size, int valueToRemove) {
         array[i] = -999;
     }
 }
+
+void ShiftArray(int array[], int size, int shiftAmount) {
+    int kSize = size;
+    int tempArray[kSize];
+    for (int i = 0; i < size; ++i) {
+        tempArray[i] = array[i];
+    }
+
+    for (int i = 0; i < size; ++i) {
+        int shiftIndex = i + shiftAmount;
+        while (shiftIndex < 0) {
+            shiftIndex = size + shiftIndex;
+        }
+        while (shiftIndex >= size) {
+            shiftIndex = shiftIndex - size;
+        }
+        array[i] = tempArray[shiftIndex];
+    }
+}
